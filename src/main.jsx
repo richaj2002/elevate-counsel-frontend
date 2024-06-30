86fd1a57278f1b03eadf106e818e53cb1a9ee2cd
richaj2002 <rjha072002@gmail.com>
2024-06-30 22:57:26 +0530

    Dependencies chnaged

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@material-tailwind/react';
 import { BrowserRouter } from 'react-router-dom';
-
-const theme = createTheme({
-  typography: {
-    fontFamily: ['Inter', 'sans-serif'].join(','),
-  },
-  palette: {
-    primary: {
-      main: '#3b82f6',
-    },
-    secondary: {
-      main: '#f43f5e',
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider>
         <App />
       </ThemeProvider>
     </BrowserRouter>
